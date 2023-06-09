9b49b13b2bbfa03f129310c83bb9ff5551b76054
PanHongRainbow <panhongrainbow@gmail.com>
2023-06-09 17:00:48 +0800

    [model]  retrieves all attributes from the log 🙂.
    Postscript:
    First retrieve all the attributes, then there won't be much of a difficulty 😁.

--- model/TracingData.go
+++ model/TracingData.go
@@ -1,65 +1,87 @@
 package model
 
-import "time"
+import (
+	"fmt"
+	"time"
+)
 
-// TracingData struct represents a distributed tracing document
+type SpanContext struct {
+	TraceID    string `json:"TraceID"`
+	SpanID     string `json:"SpanID"`
+	TraceFlags string `json:"TraceFlags"`
+	TraceState string `json:"TraceState"`
+	Remote     bool   `json:"Remote"`
+}
+
+type Attribute struct {
+	Key   string `json:"Key"`
+	Value struct {
+		Type  string      `json:"Type"`
+		Value interface{} `json:"Value"`
+	} `json:"Value"`
+}
+
+type Event struct {
+	Name                  string      `json:"Name"`
+	Attributes            []Attribute `json:"Attributes"`
+	DroppedAttributeCount int         `json:"DroppedAttributeCount"`
+	Time                  time.Time   `json:"Time"`
+}
+
+type Status struct {
+	Code        string `json:"Code"`
+	Description string `json:"Description"`
+}
+
+type InstrumentationLibrary struct {
+	Name      string `json:"Name"`
+	Version   string `json:"Version"`
+	SchemaURL string `json:"SchemaURL"`
+}
+
 type TracingData struct {
-	Name        string `json:"Name"`
-	SpanContext struct {
-		TraceID    string `json:"TraceID"`
-		SpanID     string `json:"SpanID"`
-		TraceFlags string `json:"TraceFlags"`
-		TraceState string `json:"TraceState"`
-		Remote     bool   `json:"Remote"`
-	} `json:"SpanContext"`
-	Parent struct {
-		TraceID    string `json:"TraceID"`
-		SpanID     string `json:"SpanID"`
-		TraceFlags string `json:"TraceFlags"`
-		TraceState string `json:"TraceState"`
-		Remote     bool   `json:"Remote"`
-	} `json:"Parent"`
-	SpanKind   int       `json:"SpanKind"`
-	StartTime  time.Time `json:"StartTime"`
-	EndTime    time.Time `json:"EndTime"`
-	Attributes []struct {
-		Key   string `json:"Key"`
-		Value struct {
-			Type  string `json:"Type"`
-			Value string `json:"Value"`
-		} `json:"Value"`
-	} `json:"Attributes"`
-	Events []struct {
-		Name       string `json:"Name"`
-		Attributes []struct {
-			Key   string `json:"Key"`
-			Value struct {
-				Type  string `json:"Type"`
-				Value string `json:"Value"`
-			} `json:"Value"`
-		} `json:"Attributes"`
-		DroppedAttributeCount int       `json:"DroppedAttributeCount"`
-		Time                  time.Time `json:"Time"`
-	} `json:"Events"`
-	Links  any `json:"Links"`
-	Status struct {
-		Code        string `json:"Code"`
-		Description string `json:"Description"`
-	} `json:"Status"`
-	DroppedAttributes int `json:"DroppedAttributes"`
-	DroppedEvents     int `json:"DroppedEvents"`
-	DroppedLinks      int `json:"DroppedLinks"`
-	ChildSpanCount    int `json:"ChildSpanCount"`
-	Resource          []struct {
-		Key   string `json:"Key"`
-		Value struct {
-			Type  string `json:"Type"`
-			Value string `json:"Value"`
-		} `json:"Value"`
-	} `json:"Resource"`
-	InstrumentationLibrary struct {
-		Name      string `json:"Name"`
-		Version   string `json:"Version"`
-		SchemaURL string `json:"SchemaURL"`
-	} `json:"InstrumentationLibrary"`
+	Name                   string `json:"Name"`
+	SpanContext            SpanContext
+	Parent                 SpanContext
+	SpanKind               int                    `json:"SpanKind"`
+	StartTime              time.Time              `json:"StartTime"`
+	EndTime                time.Time              `json:"EndTime"`
+	Attributes             []Attribute            `json:"Attributes"`
+	Events                 []Event                `json:"Events"`
+	Links                  any                    `json:"Links"`
+	Status                 Status                 `json:"Status"`
+	DroppedAttributes      int                    `json:"DroppedAttributes"`
+	DroppedEvents          int                    `json:"DroppedEvents"`
+	DroppedLinks           int                    `json:"DroppedLinks"`
+	ChildSpanCount         int                    `json:"ChildSpanCount"`
+	Resource               []Attribute            `json:"Resource"`
+	InstrumentationLibrary InstrumentationLibrary `json:"InstrumentationLibrary"`
+}
+
+func Unmarshal(jsonData []byte) (result TracingData) {
+	start := 0
+	attribute := 0
+
+	for ; start < len(jsonData); start++ {
+		if jsonData[start] == ',' {
+			attribute = start
+		}
+		if jsonData[start] == ':' {
+			key := string(jsonData[attribute+2 : start])
+			fmt.Println("key->", key)
+			valueStart := start + 2
+
+			for valueEnd := valueStart; valueEnd < len(jsonData); valueEnd++ {
+				if jsonData[valueEnd] == '"' {
+					value := string(jsonData[valueStart:valueEnd])
+					fmt.Println("value->", value)
+					break
+				}
+			}
+
+			attribute = start + 1
+			start = start + 1
+		}
+	}
+	return
 }
---
+++ model/TracingData_test.go
@@ -0,0 +1,10 @@
+package model
+
+import (
+	"testing"
+)
+
+func Test_Check_TracingData(t *testing.T) {
+	var jsonData = []byte(`{"Name":"functionBSpan","SpanContext":{"TraceID":"77ea467445562b1afb250147b0ddc178","SpanID":"e7d262c286f5b660","TraceFlags":"01","TraceState":"","Remote":false},"Parent":{"TraceID":"77ea467445562b1afb250147b0ddc178","SpanID":"b178c20dce680429","TraceFlags":"01","TraceState":"","Remote":false},"SpanKind":1,"StartTime":"2023-05-29T01:49:32.011159939+08:00","EndTime":"2023-05-29T01:49:33.011344186+08:00","Attributes":[{"Key":"ParameterB","Value":{"Type":"STRING","Value":"ValueB"}}],"Events":[{"Name":"exception","Attributes":[{"Key":"ID","Value":{"Type":"INT64","Value":1}},{"Key":"postscript","Value":{"Type":"STRING","Value":"more details"}},{"Key":"exception.type","Value":{"Type":"STRING","Value":"*errors.errorString"}},{"Key":"exception.message","Value":{"Type":"STRING","Value":"error"}}],"DroppedAttributeCount":0,"Time":"2023-05-29T01:49:32.011163907+08:00"}],"Links":null,"Status":{"Code":"Error","Description":"functionB failed"},"DroppedAttributes":0,"DroppedEvents":0,"DroppedLinks":0,"ChildSpanCount":0,"Resource":[{"Key":"service.name","Value":{"Type":"STRING","Value":"unknown_service:___go_build_github_com_panhongrainbow_tracez_example_openTelemetry2file"}},{"Key":"telemetry.sdk.language","Value":{"Type":"STRING","Value":"go"}},{"Key":"telemetry.sdk.name","Value":{"Type":"STRING","Value":"opentelemetry"}},{"Key":"telemetry.sdk.version","Value":{"Type":"STRING","Value":"1.14.0"}}],"InstrumentationLibrary":{"Name":"functionBTracer","Version":"","SchemaURL":""}}`)
+	Unmarshal(jsonData)
+}
